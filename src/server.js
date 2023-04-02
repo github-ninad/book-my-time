410bece548e429e5d84ac8eafe3bec02631b6b66
Ninad Kulkarni <mail.ninad@yahoo.com>
2023-04-03 00:33:14 +0530

    Cancel Appointment Wrapper

--- src/server.js
+++ src/server.js
@@ -20,7 +20,9 @@ app.get('/hello', async (req, res) => {
 });
 
 
-
+/**
+ * Schedule Appointment
+ */
 const Schedule_Appointment_Internal_HASURA_OPERATION = `
 mutation Schedule_Appointment_Internal($email: String!, $name: String!, $start_datetime: timestamptz!) {
   insert_user_one(object: {name: $name, email: $email}, on_conflict: {constraint: user_email_key, update_columns: [name, created_at]}) {
@@ -75,5 +77,63 @@ app.post('/Schedule_Appointment', async (req, res) => {
 
 });
 
+
+/**
+ * Cancel Appointment
+ * 
+ */
+const CancelAppointmentInternal_HASURA_OPERATION = `
+mutation CancelAppointmentInternal($appointmentId: uuid!) {
+  update_appointment(_set: {iscancelled: true}, where: {appointment_id: {_eq: $appointmentId}}) {
+    affected_rows
+    returning {
+      start_datetime
+      user_email
+      iscancelled
+    }
+  }
+}
+`;
+
+// execute the parent operation in Hasura
+const CancelAppointmentInternal_execute = async (variables) => {
+  const fetchResponse = await fetch(
+    "https://touching-herring-78.hasura.app/v1/graphql",
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        query: CancelAppointmentInternal_HASURA_OPERATION,
+        variables
+      })
+    }
+  );
+  const data = await fetchResponse.json();
+  console.log('DEBUG: ', data);
+  return data;
+};
+  
+
+// Request Handler
+app.post('/Cancel_Appointment', async (req, res) => {
+
+  // get request input
+  const { appointmentId } = req.body.input;
+
+  // run some business logic
+
+  // execute the Hasura operation
+  const { data, errors } = await CancelAppointmentInternal_execute({ appointmentId });
+
+  // if Hasura operation errors, then throw error
+  if (errors) {
+    return res.status(400).json(errors[0])
+  }
+
+  // success
+  return res.json({
+    ...data.update_appointment
+  })
+
+});
 
 app.listen(PORT);
